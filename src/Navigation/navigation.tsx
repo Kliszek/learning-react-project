20c9aab95cafc7e2c9ab1f755d7b16a7616fd1db
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-05-29 23:24:58 +0200

    Added hamburger menu for mobiles

--- src/Navigation/navigation.tsx
+++ src/Navigation/navigation.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { NavRouteItem, NavRoutes } from "./routes";
 //import { NavRouteItem, NavRoutes } from "./routes";
 
 export const Navigation = () => {
+  const [showNavigation, setShowNavigation] = useState<boolean>(false);
+
   return (
-    <nav className="absolute top-0 w-full bg-slate-700 border-b border-slate-600">
-      <ul className="flex flex-row justify-center gap-10">
+    <nav className="mt-0 mb-10 w-full bg-slate-700 border-b border-slate-600">
+      <ul className="flex flex-col text-sm md:flex-row md:text-lg lg:text-2xl justify-center md:gap-10">
+        <li>
+          <button className="md:hidden" onClick={()=>setShowNavigation(!showNavigation)}>
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          </button>
+        </li>
         {NavRoutes.map((item:NavRouteItem) => 
-          <li key={item.key} className="px-10 py-3 hover:underline hover:bg-slate-800 duration-200">
-            <Link to={item.address}>{item.name}</Link>
+          <li className={`${showNavigation?'block':'hidden'} md:block`} key={item.key}>
+            <Link to={item.address} className="flex flex-row items-center justify-center gap-2 px-10 py-3 hover:underline hover:bg-slate-800 duration-200">
+              {item.icon && <span>{ item.icon }</span> }
+              {item.name}
+            </Link>
           </li>
           )}
       </ul>
--- src/Navigation/routes.tsx
+++ src/Navigation/routes.tsx
@@ -6,12 +6,45 @@ export interface NavRouteItem {
   key: number;
   name: string;
   address: string;
+  icon?: JSX.Element;
   component: JSX.Element;
-};
+}
 
 export const NavRoutes: NavRouteItem[] = [
-  {key:1, name: "Home", address:"/", component: <InputForm username=''/> },
-  {key:2, name: "About", address:"/about", component: <About />},
-  {key:3, name: "Form", address:"/form", component: <InputForm username=''/>},
-  {key:4, name: "Policy", address:"/policy", component: <Policy />}
+  {
+    key: 1,
+    name: "Home",
+    address: "/",
+    icon: (
+      <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+    ),
+    component: <InputForm username="" />,
+  },
+  {
+    key: 2,
+    name: "About",
+    address: "/about",
+    icon: (
+      <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    ),
+    component: <About />,
+  },
+  {
+    key: 3,
+    name: "Form",
+    address: "/form",
+    icon: (
+      <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+    ),
+    component: <InputForm username="" />,
+  },
+  {
+    key: 4,
+    name: "Policy",
+    address: "/policy",
+    icon: (
+      <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+    ),
+    component: <Policy />
+  },
 ];
